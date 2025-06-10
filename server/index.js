8eca6e604eb16606ab2a85af40df10c3f1203b8c
shresthgupta9 <shresthgupta9@gmail.com>
2025-06-10 18:23:57 +0530

    finished initial project

--- server/index.js
+++ server/index.js
@@ -31,6 +31,15 @@ app.get('/api/config/paypal', (req, res) => res.json({ clientId: process.env.PAY
 
 const __dirname = path.resolve();
 
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '/frontend/build')));
+    app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html')));
+} else {
+    app.get('/', (req, res) => {
+        res.send('API is running...');
+    });
+}
+
 app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
 app.use(notFound);
 app.use(errorHandler);
